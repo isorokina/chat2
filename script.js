3605081fa631daa8c6da4915d6b8d74c40f42e4b
isorokina <ijasorokina@inbox.lv>
2022-07-15 17:03:02 +0300

    lab5

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //alert(1)
-console.log(1)
+//console.log(1)
+
+function suutiitZinju()
+{
+    console.log('Suutiit');
+    let zinja = document.querySelector('.manaZinja');
+    let zinjas = document.querySelector('.chataZinjas');
+   zinjas.innerHTML=zinjas.innerHTML+'<br />'+zinja.value;
+}
