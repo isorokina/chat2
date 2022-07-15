7377be8f0d83f28cab73ae8935920191b9d5d94b
isorokina <ijasorokina@inbox.lv>
2022-07-15 17:19:37 +0300

    lab6

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 //alert(1)
 //console.log(1)
 
+let zinja = document.querySelector('.manaZinja');
+let zinjas = document.querySelector('.chataZinjas');
+
 function suutiitZinju()
 {
-    console.log('Suutiit');
-    let zinja = document.querySelector('.manaZinja');
-    let zinjas = document.querySelector('.chataZinjas');
+   // console.log('Suutiit'); pārbaude
    zinjas.innerHTML=zinjas.innerHTML+'<br />'+zinja.value;
+}
+
+async function ielaadeetChataZinjas()
+{
+let datiNoServera = await fetch('chatazinjas.txt');
+let dati = await datiNoServera.text();
+zinjas.innerHTML = dati;
+//console.log(dati);
 }
