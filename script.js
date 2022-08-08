d47116bf298e33f8c2a69762a32327bfb112e565
isorokina <93764917+isorokina@users.noreply.github.com>
2022-08-08 17:51:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 ///const API="https://chat21.ijasorokina2.repl.co"
 const API="https://chat2newkursi.ijasorokina2.repl.co"
->>>>>>> f8951b0807a473fc4a8178eda63b8522dafbe1a1
 //alert(1)
 //console.log(1)
 
