264fd657a74770487b28ae4e3ebc7ea85d38ee08
isorokina <93764917+isorokina@users.noreply.github.com>
2022-08-08 17:54:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 ///const API="https://chat21.ijasorokina2.repl.co"
 const API="https://chat2newkursi.ijasorokina2.repl.co"
-
 let zinja = document.querySelector('.manaZinja');
 let zinjas = document.querySelector('.chataZinjas');
 
